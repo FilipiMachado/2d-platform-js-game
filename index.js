cebff66dac733ddc028ba8136d651334d0d5861e
fil <filipisee@hotmail.com>
2022-10-06 21:05:07 -0300

    structuring player class - refactoring

--- index.js
+++ index.js
@@ -4,14 +4,35 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16; // 1024
 canvas.height = 64 * 9; // 576
 
-c.fillStyle = "#fff";
-c.fillRect(0, 0, canvas.width, canvas.height);
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
 
-c.fillStyle = "red";
-c.fillRect(100, 100, 100, 100);
+    this.width = 100;
+    this.height = 100;
+  }
+}
 
+let y = 100;
+const height = 100;
+let bottom = y + 100;
 function animate() {
   window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  c.fillStyle = "red";
+  c.fillRect(100, y, 100, height);
+
+  if (bottom < canvas.height) {
+    y++;
+    bottom = y + 100;
+  }
+
+  console.log("animation running");
 }
 
 animate();
