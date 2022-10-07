5b112c0436cacf96ba19aea1bdc99cc9e16334e1
fil <filipisee@hotmail.com>
2022-10-06 21:18:10 -0300

    finished Player class

--- index.js
+++ index.js
@@ -4,33 +4,15 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16; // 1024
 canvas.height = 64 * 9; // 576
 
-class Player {
-  constructor() {
-    this.position = {
-      x: 100,
-      y: 100,
-    };
+const player = new Player();
 
-    this.width = 100;
-    this.height = 100;
-  }
-}
-
-let y = 100;
-const height = 100;
-let bottom = y + 100;
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "white";
   c.fillRect(0, 0, canvas.width, canvas.height);
 
-  c.fillStyle = "red";
-  c.fillRect(100, y, 100, height);
-
-  if (bottom < canvas.height) {
-    y++;
-    bottom = y + 100;
-  }
+  player.draw();
+  player.update();
 
   console.log("animation running");
 }
