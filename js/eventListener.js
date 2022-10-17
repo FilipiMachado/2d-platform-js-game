dd307d8dadc21c0ca08a66bc2f11096b0fa64862
fil <filipisee@hotmail.com>
2022-10-17 16:40:22 -0300

    finished player movement

---
+++ js/eventListener.js
@@ -0,0 +1,32 @@
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "w":
+      if (player.velocity.y === 0) player.velocity.y = -20;
+
+      break;
+    // move player to the left
+    case "a":
+      keys.a.pressed = true;
+      break;
+    // move player to the right
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case "s":
+      player.velocity.x = 0;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    // move player to the left
+    case "a":
+      keys.a.pressed = false;
+      break;
+    // move player to the right
+    case "d":
+      keys.d.pressed = false;
+      break;
+  }
+});
