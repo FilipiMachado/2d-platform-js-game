3bd1e26a55c59124f423cbeda3f851f64c34524b
fil <filipisee@hotmail.com>
2022-10-17 16:32:31 -0300

    working on player movement

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const canvas = document.getElementById("canvas");
 const c = canvas.getContext("2d");
 
-canvas.width = 64 * 16; // 1024
-canvas.height = 64 * 9; // 576
+canvas.width = 64 * 16; // 1024px
+canvas.height = 64 * 9; // 576px
 
 const player = new Player();
+
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
 
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "white";
   c.fillRect(0, 0, canvas.width, canvas.height);
 
+  player.velocity.x = 0;
+  if (keys.d.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.a.pressed) {
+    player.velocity.x = -5;
+  }
   player.draw();
   player.update();
 
@@ -27,11 +45,11 @@ window.addEventListener("keydown", (e) => {
       break;
     // move player to the left
     case "a":
-      player.velocity.x = -4;
+      keys.a.pressed = true;
       break;
     // move player to the right
     case "d":
-      player.velocity.x = 4;
+      keys.d.pressed = true;
       break;
     case "s":
       player.velocity.x = 0;
@@ -43,11 +61,11 @@ window.addEventListener("keyup", (e) => {
   switch (e.key) {
     // move player to the left
     case "a":
-      player.velocity.x = 0;
+      keys.a.pressed = false;
       break;
     // move player to the right
     case "d":
-      player.velocity.x = 0;
+      keys.d.pressed = false;
       break;
   }
 });
