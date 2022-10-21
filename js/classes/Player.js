59c9bf3fbea4a52806fe598c005360943ed2ab64
fil <filipisee@hotmail.com>
2022-10-21 14:51:25 -0300

    small adjusts

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,8 +1,8 @@
 class Player {
   constructor({ collisionBlocks = [] }) {
     this.position = {
-      x: 100,
-      y: 100,
+      x: 200,
+      y: 200,
     };
 
     this.velocity = {
@@ -10,15 +10,14 @@ class Player {
       y: 0,
     };
 
-    this.width = 100;
-    this.height = 100;
+    this.width = 25;
+    this.height = 25;
     this.sides = {
       bottom: this.position.y + this.height,
     };
     this.gravity = 1;
 
     this.collisionBlocks = collisionBlocks;
-    console.log(collisionBlocks);
   }
 
   draw() {
@@ -40,13 +39,13 @@ class Player {
         this.position.y <= collisionBlock.position.y + collisionBlock.height
       ) {
         // collision on x axis going to the left
-        if (this.velocity.x < -1) {
+        if (this.velocity.x < 0) {
           this.position.x =
             collisionBlock.position.x + collisionBlock.width + 0.01;
           break;
         }
 
-        if (this.velocity.x > 1) {
+        if (this.velocity.x > 0) {
           this.position.x = collisionBlock.position.x - this.width - 0.01;
           break;
         }
@@ -54,14 +53,39 @@ class Player {
     }
 
     // apply gravity
+    this.velocity.y += this.gravity;
     this.position.y += this.velocity.y;
-    this.sides.bottom = this.position.y + this.height;
 
-    // above bottom of canvas
+    // check for vertical collisions
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const collisionBlock = this.collisionBlocks[i];
+
+      // if a collision exists
+      if (
+        this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+        this.position.x + this.width >= collisionBlock.position.x &&
+        this.position.y + this.height >= collisionBlock.position.y &&
+        this.position.y <= collisionBlock.position.y + collisionBlock.height
+      ) {
+        if (this.velocity.y < 0) {
+          this.velocity.y = 0;
+          this.position.y =
+            collisionBlock.position.y + collisionBlock.height + 0.01;
+          break;
+        }
+
+        if (this.velocity.y > 0) {
+          this.velocity.y = 0;
+          this.position.y = collisionBlock.position.y - this.height - 0.01;
+          break;
+        }
+      }
+    }
+
+    /* // above bottom of canvas
     if (this.sides.bottom + this.velocity.y < canvas.height) {
-      this.velocity.y += this.gravity;
     } else {
       this.velocity.y = 0;
-    }
+    } */
   }
 }
